1c43312b875fe01ad3634a0deeb7cd2c96c7e1a3
karnVen <onemonth06@gmail.com>
2026-09-07 18:49:30 +0000

    fix(deploy): use fully qualified Kernel class in deploy runner

--- public/deploy_sub_agents_migration.php
+++ public/deploy_sub_agents_migration.php
@@ -26,7 +26,7 @@ define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(\Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
 use Illuminate\Contracts\Console\Kernel;
